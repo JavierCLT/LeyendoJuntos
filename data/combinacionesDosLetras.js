68cc3af1229c59056e9fe715c84a6e6e8576b038
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-10-13 10:37:31 -0400

    Update combinacionesDosLetras.js

--- data/combinacionesDosLetras.js
+++ data/combinacionesDosLetras.js
@@ -1,1 +1,5 @@
-
+export const combinacionesDosLetras = {
+  vc: ['al', 'an', 'ar', 'as', 'el', 'em', 'en', 'er', 'es', 'id', 'im', 'in', 'ir', 'is', 'ob', 'ol', 'om', 'on', 'or', 'os', 'ul', 'un', 'ur'],
+  cv: ['ba', 'be', 'bi', 'bo', 'bu', 'ca', 'ce', 'ci', 'co', 'cu', 'da', 'de', 'di', 'do', 'du', 'fa', 'fe', 'fi', 'fo', 'fu', 'ga', 'ge', 'gi', 'go', 'gu', 'ha', 'he', 'hi', 'ho', 'hu', 'ja', 'je', 'ji', 'jo', 'ju', 'ka', 'ke', 'ki', 'ko', 'ku', 'la', 'le', 'li', 'lo', 'lu', 'ma', 'me', 'mi', 'mo', 'mu', 'na', 'ne', 'ni', 'no', 'nu', 'pa', 'pe', 'pi', 'po', 'pu', 'que', 'qui', 'ra', 're', 'ri', 'ro', 'ru', 'sa', 'se', 'si', 'so', 'su', 'ta', 'te', 'ti', 'to', 'tu', 'va', 've', 'vi', 'vo', 'vu', 'xa', 'xe', 'xi', 'xo', 'xu', 'ya', 'ye', 'yi', 'yo', 'yu', 'za', 'ce', 'ci', 'zo', 'zu'],
+  vv: ['ai', 'au', 'ei', 'eu', 'ia', 'ie', 'io', 'iu', 'oi', 'ou', 'ua', 'ue', 'ui', 'uo'],
+};
