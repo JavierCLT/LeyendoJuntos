de97854ea0c4765c93d9dcffa71da26491c878df
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-10-13 10:46:35 -0400

    Create colorMap.js

---
+++ data/colorMap.js
@@ -0,0 +1,7 @@
+export const consonantColorMap = {
+  'b': '#c1121f', 'c': '#2a9d8f', 'd': '#e9c46a', 'f': '#219ebc', 'g': '#f4a261',
+  'h': '#2a9d8f', 'j': '#fc8181', 'k': '#9ae6b4', 'l': '#63b3ed', 'm': '#faf089',
+  'n': '#d6bcfa', 'p': '#fbd38d', 'q': '#90cdf4', 'r': '#f687b3', 's': '#b2f5ea',
+  't': '#fbb6ce', 'v': '#c6f6d5', 'w': '#feb2b2', 'x': '#b794f4', 'y': '#fbd38d',
+  'z': '#90cdf4', 'ñ': '#f687b3'
+};
