131d0c41ea85cb3fb188ac5fc62f560451819adc
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2024-10-13 11:26:17 -0400

    Update colorMap.js

--- data/colorMap.js
+++ data/colorMap.js
@@ -1,7 +1,7 @@
 export const consonantColorMap = {
   'b': '#c1121f', 'c': '#2a9d8f', 'd': '#e9c46a', 'f': '#219ebc', 'g': '#f4a261',
   'h': '#2a9d8f', 'j': '#fc8181', 'k': '#9ae6b4', 'l': '#63b3ed', 'm': '#faf089',
-  'n': '#d6bcfa', 'p': '#fbd38d', 'q': '#90cdf4', 'r': '#f687b3', 's': '#b2f5ea',
-  't': '#fbb6ce', 'v': '#c6f6d5', 'w': '#feb2b2', 'x': '#b794f4', 'y': '#fbd38d',
+  'n': '#d6bcfa', 'p': '#fbd38d', 'q': '#90cdf4', 'r': '#f687b3', 's': '#c1121f',
+  't': '#f4a261', 'v': '#e76f51', 'w': '#feb2b2', 'x': '#b794f4', 'y': '#e9c46a',
   'z': '#90cdf4', 'ñ': '#f687b3'
 };
