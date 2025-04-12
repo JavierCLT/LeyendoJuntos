b5baacec1d4e7ba3885a1e88c06ee814443fbbb0
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2025-04-11 22:20:23 -0400

    Update frasesNivel4.js

--- data/frasesNivel4.js
+++ data/frasesNivel4.js
@@ -15,5 +15,18 @@ export const frasesNivel4 = [
   'Los peces viven en el agua', 'La música suena alto', 'El caballo corre en el campo', 'La luna está llena',
   'Mi mamá compra frutas', 'El reloj marca las horas', 'Hoy es un buen día', 'Los juguetes están guardados',
   'Mi casa tiene un balcón', 'El tren pasa por el puente', 'El pastel está delicioso', 'Las ventanas están limpias',
-  'Mi perro tiene una pelota roja', 'El río es largo y ancho', 'La noche está tranquila'
+  'Mi perro tiene una pelota roja', 'El río es largo y ancho', 'La noche está tranquila', 'Madrid es la capital de España',
+  'El fuego quema', 'El agua se congela a cero grados', 'El corazón late todo el día', 'Los peces respiran bajo el agua', 
+  'El Sol es una estrella', 'La Tierra gira alrededor del Sol', 'Las abejas fabrican miel', 'La jirafa es el animal más alto', 
+  'Los pingüinos viven en lugares fríos', 'El elefante tiene trompa', 'Murciélago tiene las cinco vocales', 
+  'El camello guarda agua en su joroba', 'Las estrellas salen de noche', 'La Luna no tiene luz propia', 
+  'El canguro lleva a su bebé en una bolsa', 'Los delfines son muy inteligentes', 'Las hormigas pueden cargar cosas muy pesadas', 
+  'El tiburón tiene muchos dientes', 'Las plantas necesitan agua y sol', 'El arcoíris tiene muchos colores', 
+  'El desierto tiene mucha arena', 'Los volcanes lanzan lava caliente', 'El hielo flota en el agua', 'Las ranas nacen en el agua', 
+  'El plátano tiene potasio', 'El azúcar es muy dulce', 'Los pulpos tienen ocho brazos', 'El maíz crece en el campo', 
+  'El koala duerme casi todo el día', 'El tren se mueve sobre raíles', 'El Sol calienta la Tierra', 'El cactus guarda agua en su interior', 
+  'Los lobos viven en manada', 'El tomate es una fruta', 'La cebolla crece bajo tierra', 'El viento mueve los molinos', 
+  'Los planetas giran alrededor del Sol', 'El oro es un metal valioso', 'Los árboles limpian el aire', 
+  'El chocolate se hace con cacao', 'El trueno suena después del relámpago', 'Las tortugas pueden vivir cien años', 
+  'Las nubes están hechas de agua'.
 ];
